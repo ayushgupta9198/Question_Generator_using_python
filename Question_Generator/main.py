14953a40787b587390b46d8fc887d7cdd5393a35
Ayush Gupta <ayush227316@gmail.com>
2022-02-08 11:41:50 +0530

    code_file

---
+++ Question_Generator/main.py
@@ -0,0 +1,27 @@
+import aqgFunction
+
+
+# Main Function
+def main():
+    # Create AQG object
+    aqg = aqgFunction.AutomaticQuestionGenerator()
+
+    inputTextPath = "/content/drive/MyDrive/Ayush_Gupta/comment_generator/Automatic-Question-Generator/AutomaticQuestionGenerator/DB/db.txt"
+    readFile = open(inputTextPath, 'r+', encoding="utf8")
+    #readFile = open(inputTextPath, 'r+', encoding="utf8", errors = 'ignore')
+
+    inputText = readFile.read()
+    #inputText = '''I am Dipta. I love codding. I build my carrier with this.'''
+
+    questionList = aqg.aqgParse(inputText)
+    aqg.display(questionList)
+
+    #aqg.DisNormal(questionList)
+
+    return 0
+
+
+# Call Main Function
+if __name__ == "__main__":
+    main()
+
